5bb3273f3e4fefbb68a61b8a31f65cd449f12d44
Doggy-test <1827283822@qq.com>
2022-01-18 14:39:54 +0800

    Create test.go

---
+++ test.go
@@ -0,0 +1,3 @@
+read.jdbc.url=jdbc:mysql://r.nbapidw.p.mysql.elong.com:6082/nbapidw?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false
+read.jdbc.username=nbapidw_1
+read.jdbc.password=jYoVgV6gm2dU
