34bf536f0ade99ef5acd0b959e7abcabedad4b19
Doggy-test <1827283822@qq.com>
2022-01-20 10:46:19 +0800

    Update test.go

--- test.go
+++ test.go
@@ -1,3 +1,1 @@
-read.jdbc.url=jdbc:mysql://r.nb2w.p.mysql.elong.com:6082/nb2w?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false
-read.jdbc.username=nbapidw_1
-read.jdbc.password=jYo0216gm2db
+test233
